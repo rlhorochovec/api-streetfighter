9967edd8f21328e9ddf9972ede6b8352a1236f32
Rafael Horochovec <rl.horochovec@gmail.com>
2020-12-03 17:51:39 -0300

    Configuração do banco de dados

--- config/db.config.js
+++ config/db.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-    HOST: "ec2-54-236-169-55.compute-1.amazonaws.com",
-    USER: "nvyszotoiwfcfq",
-    PASSWORD: "a757bf6a5554b5986cc0ce62ac8451459676c551f653b252b5982de97b93dcd3",
-    DB: "d58u097nedu8nm",
-    // HOST: "localhost",
-    // USER: "postgres",
-    // PASSWORD: "admin",
-    // DB: "apistreetfighter",
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "admin",
+    DB: "streetfighter",
     dialect: "postgres",
     pool: {
         max: 5,
